564d090dd4fdec983dfb23a17499e8559c520935
Meg Ducharme <ducharme.meg@gmail.com>
2016-09-14 18:25:59 -0500

    Tracking Preferences

--- app/controllers/UserPreferencesCtrl.js
+++ app/controllers/UserPreferencesCtrl.js
@@ -3,31 +3,28 @@
 app.controller("UserPreferencesCtrl", function ($scope, EventFactory, $location) {
 
 
-$scope.checkboxModel = {
-  coffee:false,
-  waterfallHike:false,
-  value3:false,
-  value4:false,
-  value5:false,
-  value6:true,
-  value7:false,
-  value8:true,
-  value9:false
-}
+$scope.showPreferredEvents = () => {
 
-
-values = [];
-
-for(var value in $scope.checkboxModel) {
-  if (value) {
-  values.push(value);
-  console.log(values);
+  $scope.checkboxModel = {
+    coffee:false,
+    waterfallHike:false,
+    breweries:false,
+    liveMusic:false,
+    tours:false,
+    placesToEat:false,
+    cityParks:false,
+    getDrinks:false,
+    shopping:false,
   }
-}
 
+  values = [];
+  for(var value in $scope.checkboxModel) {
+    if (value) {
+    values.push(value);
+    console.log(values);
+    }
+  }
 
-$scope.addPreferencesToObject = () => {
-  EventFactory.addPreferences();
-}
+};
 
 });
