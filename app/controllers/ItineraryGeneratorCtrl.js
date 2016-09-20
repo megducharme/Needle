e16fb809fc335b31ccaf6062bf72f1af9010d490
Meg Ducharme <ducharme.meg@gmail.com>
2016-09-20 15:13:57 -0500

    Can almost generate an itnerary

--- app/controllers/ItineraryGeneratorCtrl.js
+++ app/controllers/ItineraryGeneratorCtrl.js
@@ -16,32 +16,65 @@ $scope.$parent.getUser()
   .catch(() => console.error);
 
 
+// $scope.getAllEvents = () => {
+//   EventFactory.getTimeAllottedEvents("halfDay")
+//     .then( (events) => {
+//       Object.keys(events).forEach((key) =>{
+//         events[key].id = key;
+//         halfDay.push(events[key])
+//       });
+//       console.log("halfDay", halfDay);
+//     });
+
+//   EventFactory.getTimeAllottedEvents("fullDay")
+//     .then ( ( events) => {
+//       Object.keys(events).forEach((key) =>{
+//         events[key].id = key;
+//         fullDay.push(events[key])
+//       });
+//     });
+
+//   EventFactory.getTimeAllottedEvents("fewHours")
+//     .then ( (events) => {
+//       Object.keys(events).forEach((key) =>{
+//         events[key].id = key;
+//         fewHours.push(events[key])
+//       });
+//       console.log("few hours length before promise", fewHours.length);
+//     });
+//   generateItinerary();
+// };
+
 $scope.getAllEvents = () => {
-  EventFactory.getTimeAllottedEvents("halfDay")
-    .then( (events) => {
-      for(var event in events){
-        halfDay.push(event);
-      }
-    });
+  Promise.all ([
+    EventFactory.getTimeAllottedEvents("halfDay"),
+    EventFactory.getTimeAllottedEvents("fewHours"),
+    EventFactory.getTimeAllottedEvents("fullDay")
+    ]).then(function(data) {
+      console.log("data from promise all", data);
+      extractDataFromPromiseAll(data);
+    })
+}
 
-  EventFactory.getTimeAllottedEvents("fullDay")
-    .then ( ( events) => {
-      for(var event in events){
-        fullDay.push(event);
-      }
-    });
-
-  EventFactory.getTimeAllottedEvents("fewHours")
-    .then ( (events) => {
-      for(var event in events){
-        fewHours.push(event);
-      }
-    });
-  generateItinerary();
-};
-
+function extractDataFromPromiseAll(data) {
+  Object.keys(data[0]).forEach((key) =>{
+    data[0][key].id = key;
+    fullDay.push(data[0][key])
+  })
+  console.log("full day array!?", fullDay);
+  Object.keys(data[1]).forEach((key) =>{
+    data[1][key].id = key;
+    fewHours.push(data[1][key])
+  })
+  Object.keys(data[2]).forEach((key) =>{
+    data[2][key].id = key;
+    halfDay.push(data[2][key])
+  })
+generateItinerary();
+}
 
 function generateItinerary (){
+    console.log("few hours length after promise", fewHours.length);
 let userTime = $scope.userTime;
 let options;
 console.log("user time", userTime);
@@ -50,6 +83,7 @@ console.log("user time", userTime);
     // console.log("inside switch statement");
     case "fewHours":
       let num = getRandom(fewHours.length);
+      console.log("fewHours length", fewHours.length);
       console.log("random number for few hour case", num);
       let fewHoursResult = fewHours[num];
       console.log("one event for few hours", fewHoursResult);
@@ -58,43 +92,60 @@ console.log("user time", userTime);
 
     case "halfDay":
       options = getRandom(1);
+      console.log("options should be random", options);
+      console.log("options", options);
       let halfDayResult = [];
         if(options === 0) {
-          for (var i = options; i >= 0; i--) {
+          for (var i = 1; i >= 0; i--) {
             var fewHoursRandom = getRandom(fewHours.length);
+            console.log("few hours length in half day", fewHours.length);
             fewHours.splice[fewHours[fewHoursRandom], 1];
             halfDayResult.push(fewHours[fewHoursRandom]);
+            console.log("halfDayResults in half day", halfDayResult);
           }
         }else {
           let num = getRandom(halfDay.length);
+          console.log("few hours length in half day", fewHours.length);
+
           let halfDayResults = halfDay[num];
           halfDayResult.push(halfDayResults)
+          console.log("halfDayResult in half day", halfDayResult);
           }
       break;
 
     case "fullDay":
       options = getRandom(2);
+      // console.log("options in full day should be random", options);
       let fullDayResult = [];
         if(options === 0) {
+          console.log("option 0 was randomly genereated");
+          console.log("halfday length", halfDay.length);
           let num = getRandom(halfDay.length);
+          console.log("num", num);
           let halfDayResult = halfDay[num];
+          console.log("halfday result for full day itinerary", halfDayResult);
           fullDayResult.push(halfDay[num]);
-            for (var i = options; i >= 0; i--) {
+            for (var i = 1; i >= 0; i--) {
+              console.log("is this loop happening twice?");
               var fewHoursRandom = getRandom(fewHours.length);
-              fewHours.splice[fewHours[fewHoursRandom], 1];
+              fewHours.splice(fewHours[fewHoursRandom], 1);
+              console.log("few hours event to splice", fewHours[fewHoursRandom]);
               fullDayResult.push(fewHours[fewHoursRandom]);
             }
         }else if(options === 1) {
-            for (var j = options; j >= 0; j--) {
-              var fewHoursRandom = getRandom(fewHours.length);
-              fewHours.splice[fewHours[fewHoursRandom], 1];
-              fullDayResult.push(fewHours[fewHoursRandom]);
+          console.log("option 1 was randomly genereated");
+            for (var j = 1; j >= 0; j--) {
+              var halfDayRandom = getRandom(halfDay.length);
+              halfDay.splice(halfDay[halfDayRandom], 1);
+              fullDayResult.push(halfDay[halfDayRandom]);
             }
         }else {
+          console.log("option 2 was randomly genereated");
           let num = getRandom(fullDay.length);
           let fullDayRandom = fullDay[num];
           fullDayResult.push(fullDay[num]);
         }
+        console.log("full day result!", fullDayResult);
   }
 };
 
