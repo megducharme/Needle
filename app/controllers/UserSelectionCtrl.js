eb4521269494762e72035c24defd485a6db819d2
Meg Ducharme <ducharme.meg@gmail.com>
2017-12-05 21:43:13 -0600

    crazy flattenObj functionedit

--- app/controllers/UserSelectionCtrl.js
+++ app/controllers/UserSelectionCtrl.js
@@ -6,37 +6,27 @@ console.log("USER SELECTION LOADED");
 
 let userId;
 
-let preferencesCoffee = [];
-let preferencesBreweries = [];
-let preferencesWaterfallHikes = [];
-let preferencesLiveMusic= [];
-let preferencesTours = [];
-let preferencesRestaurants = [];
-let preferencesCityPark = [];
-let preferencesGetDrinks = [];
-let preferencesShopping = [];
-
-
 $scope.$parent.getUser()
-  .then ( (user) => {
-    userId = user;
-    showEvents();
-    console.log("should have go to showEvents by now");
-  })
-  .catch(() => console.error);
+.then ( (user) => {
+  userId = user;
+  showEvents();
+  console.log("should have go to showEvents by now");
+})
+.catch(() => console.error);
 
 
 function showEvents() {
   EventFactory.getUserObject(userId)
-    .then ( (user) => {
-      console.log("this needs to be one user", user);
-      for(var key in user){
-        $scope.preferences = user[key].preferences;
-      }
-      console.log("$scope.preferences in the loop", $scope.preferences);
-      getAllPreferredEvents();
-    });
+  .then ( (user) => {
+    console.log("this needs to be one user", user);
+    for(var key in user){
+      $scope.preferences = user[key].preferences;
+    }
+    console.log("$scope.preferences in the loop", $scope.preferences);
+    getAllPreferredEvents();
+  });
 }
+
 
 function getAllPreferredEvents() {
   var promises = [];
@@ -52,122 +42,30 @@ function getAllPreferredEvents() {
 
 
 function flattenObjects(data) {
-
-  console.log("data to be flattened", data);
-  data.forEach(function(object) {
-
-    for (var singleObject in object){
-      if (object[singleObject].type === "coffee") {
-        preferencesCoffee.push(object[singleObject]);
-        console.log("coffee objects to push", object[singleObject]);
-      }
-      if (object[singleObject].type === "breweries") {
-        preferencesBreweries.push(object[singleObject]);
-      }
-      if (object[singleObject].type === "liveMusic") {
-        preferencesLiveMusic.push(object[singleObject]);
-      }
-      if (object[singleObject].type === "tours") {
-        preferencesTours.push(object[singleObject]);
-      }
-      if (object[singleObject].type === "restaurants") {
-        preferencesRestaurants.push(object[singleObject]);
-      }
-      if (object[singleObject].type === "cityPark") {
-        preferencesCityPark.push(object[singleObject]);
-      }
-      if (object[singleObject].type === "waterfallHikes") {
-        preferencesWaterfallHikes.push(object[singleObject]);
-      }
-      if (object[singleObject].type === "shopping") {
-        preferencesShopping.push(object[singleObject]);
-      }
-      console.log("should be coffee array of objects", preferencesCoffee);
+  let preferences = {}
+  console.log(data)
+  data.forEach(obj => {
+    let preferencesKey = obj[Object.keys(obj)[0]].type
+    preferences[preferencesKey] = []
+    for(let key in obj){
+      let preferenceKey = obj[key].type
+      preferences[preferencesKey].push(obj[key])
+      console.log(preferences)
     }
-
-    buildObjectsForDom();
-  });
+  })
+    buildObjectsForDom(preferences);
 }
 
-function buildObjectsForDom() {
-  console.log("inside the build objects for dom function");
-  $scope.objectsForDOM = [];
-  console.log("coffee array inside building an object", preferencesCoffee);
 
-  if(preferencesCoffee.length >= 1) {
-    let coffeeObject = {
-      eventType:"Coffee Shops",
-      events: preferencesCoffee
-    };
-    console.log("COFFEE OBJECT IS RIGHT HERE", coffeeObject);
-    $scope.objectsForDOM.push(coffeeObject);
-    console.log("scope objs for the dom THIS ONE", $scope.objectsForDOM);
+function buildObjectsForDom(preferences) {
+  $scope.objectsForDOM = [];
+
+  for(let key in preferences){
+    $scope.objectsForDOM.push({
+      eventType: key,
+      events: preferences[key]
+    })
   }
-  if(preferencesBreweries.length >= 1) {
-    let breweriesObject = {
-      eventType:"Breweries",
-      events: preferencesBreweries
-    };
-    $scope.objectsForDOM.push(breweriesObject);
-    console.log("breweries obj", breweriesObject);
-  }
-  if(preferencesWaterfallHikes.length >= 1) {
-    let waterfallHikesObject = {
-      eventType:"Waterfall Hikes",
-      events: preferencesWaterfallHikes
-    };
-    console.log("waterfall obj", waterfallHikesObject);
-    $scope.objectsForDOM.push(waterfallHikesObject);
-  }
-  if(preferencesLiveMusic.length >= 1) {
-    let liveMusicObject = {
-      eventType:"Live Music",
-      events: preferencesLiveMusic
-    };
-    console.log("live music obj", liveMusicObject);
-    $scope.objectsForDOM.push(liveMusicObject);
-  }
-  if(preferencesTours.length >= 1) {
-    let toursObject = {
-      eventType:"Tours",
-      events: preferencesTours
-    };
-    console.log("tours obj", toursObject);
-    $scope.objectsForDOM.push(toursObject);
-  }
-  if(preferencesRestaurants.length >= 1) {
-    let restaurantObject = {
-      eventType:"Restaurants",
-      events: preferencesRestaurants
-    };
-    console.log("restaurants obj", restaurantObject);
-    $scope.objectsForDOM.push(restaurantObject);
-  }
-  if(preferencesCityPark.length >= 1) {
-    let cityParkObject = {
-      eventType:"City Parks",
-      events: preferencesCityPark
-    };
-    console.log("city parks obj", cityParkObject);
-    $scope.objectsForDOM.push(cityParkObject);
-  }
-  if(preferencesGetDrinks.length >= 1) {
-    let getDrinksObject = {
-      eventType:"Get Drinks",
-      events: preferencesGetDrinks
-    };
-    console.log("drinks obj", getDrinksObject);
-    $scope.objectsForDOM.push(getDrinksObject);
-  }
-  if(preferencesShopping.length >= 1) {
-    let shoppingObject = {
-      eventType:"Shopping",
-      events: preferencesShopping
-    };
-    console.log("shopping obj", shoppingObject);
-    $scope.objectsForDOM.push(shoppingObject);
-  }
-console.log("objects for DOM", $scope.objectsForDOM);
 }
 
 $scope.saveEventToProfileVisited = (event) => {
@@ -180,6 +78,7 @@ $scope.saveEventToProfileVisited = (event) => {
       $scope.eventsVisited = response;
     });
 };
+
 
 $scope.saveEventToProfileNotVisited = (event) => {
   console.log("saving an event to profile event", event);
